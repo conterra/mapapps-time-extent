454429aec7fe1905da7237a1fc51a36d772e83d6
matthiasstein <peterCoded1!>
2019-06-07 11:35:55 +0200

    Reformat code

--- src/main/js/bundles/dn_timeextent/TimeExtentWidgetFactory.js
+++ src/main/js/bundles/dn_timeextent/TimeExtentWidgetFactory.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2019 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import TimeExtentWidget from "./TimeExtentWidget.vue";
 import Vue from "apprt-vue/Vue";
 import VueDijit from "apprt-vue/VueDijit";
@@ -19,7 +34,7 @@ export default class TimeExtentWidgetFactory {
         widget.i18n = this._i18n.get().ui;
 
         // listen to view model methods
-        vm.$on('startup', () => {
+        widget.$on('startup', () => {
 
         });
         widget.$on('setFilter', () => {
